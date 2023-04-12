c3298a7577af606fb56f71238c5eca986997b206
Yury <matatov1989@gmail.com>
2023-04-12 17:29:27 +0300

    creating a note entity

--- app/src/main/java/com/example/jetnoteapp/model/Note.kt
+++ app/src/main/java/com/example/jetnoteapp/model/Note.kt
@@ -1,11 +1,15 @@
 package com.example.jetnoteapp.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.time.LocalDateTime
 import java.util.UUID
 
+@Entity(tableName = "notes_tbl")
 data class Note(
-    val id: UUID = UUID.randomUUID(),
-    val title: String,
-    val description: String,
-    val entryDate: LocalDateTime = LocalDateTime.now()
+    @PrimaryKey val id: UUID = UUID.randomUUID(),
+    @ColumnInfo(name = "note_title") val title: String,
+    @ColumnInfo(name = "note_description") val description: String,
+    @ColumnInfo(name = "note_entry_date")val entryDate: LocalDateTime = LocalDateTime.now()
 )
